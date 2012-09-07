71d3c320012a85790123a91d9cf88723b52321ab
Prakash Vasudevan <premprakash90@gmail.com>
2012-09-07 16:25:45 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PostPress::Application.config.secret_token = '3fb50220e79551db945e3461fdcbc35c61b20e7ebe2f13bccdad87ac3b97d61b0aaedd9668550b7f6317ffd5e2e42a488d11c704b96d53cc27b0c9597bb1ae9c'
